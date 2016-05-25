c31c329fb22a361039abb179f21ec03e260013dd
Nikolaj Lauridsen <nikolajlauridsen@protonmail.ch>
2016-05-25 13:10:40 +0200

    Tweaking UI, adding keybindings and default values

--- SSTgoneGUI.py
+++ SSTgoneGUI.py
@@ -35,23 +35,30 @@ hours = StringVar()
 minutes = StringVar()
 
 # Widgets go here
-ttk.Label(mainframe, text="Simple Sleep Timer").grid(column=2, row=1, sticky=N)
+ttk.Label(mainframe, text="Simple Sleep Timer").grid(column=3, row=1, sticky=N)
 
 # Hours Entry
-ttk.Label(mainframe, text="Hours:").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="Hours:").grid(column=2, row=2, sticky=W)
 hours_entry = ttk.Entry(mainframe, width=5, textvariable=hours)
-hours_entry.grid(column=2, row=2, sticky=W)
+hours_entry.grid(column=2, row=2, sticky=E)
 
 # Minutes Entry
-ttk.Label(mainframe,text="Minutes:").grid(column=3, row=2, sticky=E)
+ttk.Label(mainframe, text="Minutes:").grid(column=3, row=2, sticky=W)
 minutes_entry = ttk.Entry(mainframe, width=5, textvariable=minutes)
-minutes_entry.grid(column=4, row=2, sticky=W)
+minutes_entry.grid(column=3, row=2, sticky=E)
 
 # Shutdown Buttons
 ttk.Button(mainframe, text="Shutdown", command=run_timer).grid(column=2, row=3, sticky=S)
 ttk.Button(mainframe, text="Restart", command=reboot).grid(column=3, row=3, sticky=S)
 ttk.Button(mainframe, text="Cancel Shutdown", command=cancel_shutdown).grid(column=4, row=3, sticky=S)
 
+
 for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+
+hours_entry.focus()
+hours_entry.insert(0, "1")
+minutes_entry.insert(0, "30")
+root.bind('<Return>', run_timer)
+root.bind('<Escape>', cancel_shutdown)
 
 root.mainloop()
